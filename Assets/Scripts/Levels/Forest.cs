9065b445a6ece710b5813d89aa26d904ab328f57
Dan Ward <dan.steve@hotmail.co.uk>
2017-01-27 20:18:19 +0000

    Started forest

---
+++ Assets/Scripts/Levels/Forest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Forest : MonoBehaviour {
+
+    //LArge map sent in, outside is blocked.
+    //Fill outside with trees
+    //Large depth, but random it a little, and make it smooth.
+    //Sparse/dense setting
+
+    private int[,] map;
+
+    public int[,] genMap()
+    {
+        int x = map.GetLength(0);
+        int y = map.GetLength(1);
+
+        return map;
+    }
+
+    public void setMap(int[,] m)
+    {
+        map = m;
+    }
+}
