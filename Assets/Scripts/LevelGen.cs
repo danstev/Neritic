594eabc8630573118301221a68b4a5e474cbd6b6
Dan Ward <dan.steve@hotmail.co.uk>
2017-02-11 16:24:44 +0000

    Gen planning

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -105,6 +105,52 @@ public class LevelGen : MonoBehaviour {
         }
     }
 
+    void drawMap()
+    {
+        //0 Empty
+        //1 Brick floor
+        //2 Cave floor
+        //3 Forest floor
+        //4 Underwater floor
+        //5 Water
+        //6 Lava
+        //7 Blood
+        //8 Poison
+        //9 Brick wall
+        //10 Cave wall
+        //11 Forest wall
+        //12
+        //13
+        //14
+        //16
+        //16
+        //17
+        //18
+        //19 Entrance
+        //20 Exit
+
+        for (int x = 0; x < map.GetLength(0); x++)
+        {
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                if (map[x, y] == 0)
+                {
+                    Vector3 mapPos = new Vector3(x * spaceMod, 0 + Random.Range(0, 0.25f), y * spaceMod);
+                    GameObject j = Instantiate(tile, mapPos, Quaternion.identity) as GameObject;
+                    j.transform.localScale = new Vector3(spaceMod, spaceMod, spaceMod);
+                }
+                else if (map[x, y] == 10)
+                {
+                    Vector3 mapPos = new Vector3(x * spaceMod, 1, y * spaceMod);
+                    Quaternion q = new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+                    GameObject j = Instantiate(tree, mapPos, Quaternion.identity) as GameObject;
+                    j.transform.localScale = new Vector3(spaceMod + Random.Range(0.0f, 0.5f), spaceMod * 2 + Random.Range(0.0f, 0.5f), spaceMod + Random.Range(0.0f, 0.5f));
+                    j.transform.rotation = q;
+                }
+            }
+        }
+    }
+
 
 }
 
