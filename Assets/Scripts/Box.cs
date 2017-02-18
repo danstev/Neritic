20e5aa68ec4416cfc1e5215a05ef51778afed17c
Dan Ward <dan.steve@hotmail.co.uk>
2017-02-18 13:43:04 +0000

    Chest started

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : MonoBehaviour {
+
+    Inventory i;
+
+    void Start()
+    {
+        i = GetComponent<Inventory>();
+    }
+
+    void worldUse()
+    {
+        //For item in inventory, detatch from parent, make real
+
+        foreach (GameObject g in i.slots)
+        {
+            //Detatch from parent
+
+            //activate the object
+
+            //
+        }
+
+        //Destroy chest
+        Destroy(this);
+    }
+}
