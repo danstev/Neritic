4a06cc8037c9105fa03a78727440a01d7fd39d81
Dan Ward <dan.steve@hotmail.co.uk>
2017-02-19 15:44:37 +0000

    Billboard + box stuff

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -3,22 +3,9 @@ using System.Collections;
 
 public class Billboard : MonoBehaviour
 {
-    private SpriteRenderer r;
-    public Sprite front;
-    public Sprite side;
-    public Sprite back;
-    public bool rotate;
-
-    void Start()
-    {
-        r = GetComponent<SpriteRenderer>();
-    }
 
     void Update() 
 	{
         transform.LookAt( new Vector3 (Camera.main.transform.position.x, 0.5f, Camera.main.transform.position.z), Vector3.up);
-
-
-
     }
 }
---
+++ Assets/Scripts/BillboardSprite.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardSprite : MonoBehaviour {
+
+    private SpriteRenderer r;
+    public Sprite front;
+    public Sprite side;
+    public Sprite back;
+    public bool rotate;
+
+    void Start()
+    {
+        r = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float enemyAngle = Vector2.Angle(new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.z), new Vector2(transform.forward.x, transform.forward.z));
+        //Only returns from 0 - 180, so don't use if you are wanting to use different sprite for sides;
+        //fuck this won't work for me;
+
+        if(enemyAngle < 60)
+        {
+            r.sprite = front;
+        }
+        else if(enemyAngle > 60 && enemyAngle < 120)
+        {
+            r.sprite = side;
+        }
+        else if(enemyAngle > 120)
+        {
+            r.sprite = back;
+        }
+
+    }
+}
