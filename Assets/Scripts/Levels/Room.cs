9de59bca491f1b913a7d96862b547adb1e012b03
Dan Ward <dan.steve@hotmail.co.uk>
2017-02-26 20:24:51 +0000

    Room started

---
+++ Assets/Scripts/Levels/Room.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room {
+
+    public Tile[] tiles;
+    Tile coords;
+    int height;
+    int width;
+
+    //Corridor stuff
+    bool horizontal; //is false, vertical
+
+}
