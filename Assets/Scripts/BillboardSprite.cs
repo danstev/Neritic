6087538c6579eb10f6ae6bfa784202a96282920c
Dan Ward <dan.steve@hotmail.co.uk>
2017-03-01 18:43:34 +0000

    bb

--- Assets/Scripts/BillboardSprite.cs
+++ Assets/Scripts/BillboardSprite.cs
@@ -16,6 +16,8 @@ public class BillboardSprite : MonoBehaviour {
 
     void Update()
     {
+        // http://answers.unity3d.com/questions/672097/doom-like-angle-based-sprite-changing.html
+        Vector2 cameraPostion = new Vector2(Camera.main.transform.forward.x, Camera.main.transform.forward.z);
         float enemyAngle = Vector2.Angle(new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.z), new Vector2(transform.forward.x, transform.forward.z));
         //Only returns from 0 - 180, so don't use if you are wanting to use different sprite for sides;
         //fuck this won't work for me;
