fef1c06c7d965af41081aec42ee4f5cc0fdf3470
Dan Ward <dan.steve@hotmail.co.uk>
2017-03-02 22:26:27 +0000

    Tiles figured

--- Assets/Scripts/TileTest.cs
+++ Assets/Scripts/TileTest.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class TileTest : MonoBehaviour {
 
     Tile t;
+    public GameObject tile;
 	// Use this for initialization
+
 	void Start () {
         t = GetComponent<Tile>();
+        t.floorTile = tile;
+        t.xPosition = 1;
+        t.yPosition = 1;
         t.render();
 
 	
