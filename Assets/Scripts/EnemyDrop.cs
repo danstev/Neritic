d9727ea63abeb2861db08d4dedcb6f84668c7303
Dan Ward <dan.steve@hotmail.co.uk>
2017-03-26 13:30:19 +0100

    ENemy drop added public items, need to fix chances

--- Assets/Scripts/EnemyDrop.cs
+++ Assets/Scripts/EnemyDrop.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class EnemyDrop : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject drop1;
+    public GameObject drop2;
+    public GameObject drop3;
+    public GameObject rareDrop;
+    public GameObject veryRareDrop;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
@@ -12,4 +18,52 @@ public class EnemyDrop : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    void OnDestroy()
+    {
+        print("Item Drop");
+        float rate = Random.Range(0f,1f);
+        print(rate);
+        if(rate < 0.5f)
+        {
+            //Drop nothing
+        }
+        else if(rate < 0.95f)
+        {
+            //Drop a normal drop
+            int item = Random.Range(0,3);
+            print(item);
+            if(item == 1)
+            {
+                Instantiate(drop1, transform.position, transform.localRotation);
+                print(drop1.name);
+            }
+            else if(item == 2)
+            {
+                Instantiate(drop2, transform);
+                print(drop2.name);
+            }
+            else if(item == 3)
+            {
+                Instantiate(drop3, transform);
+                print(drop3.name);
+            }
+        }
+        else if(rate > 0.95f)
+        {
+            //drop rare
+            Instantiate(rareDrop, transform);
+            print(rareDrop.name);
+        }
+        else if(rate > 0.98f)
+        {
+            //drop very rare
+            Instantiate(veryRareDrop, transform);
+            print(veryRareDrop.name);
+        }
+        else
+        {
+            //Drop nothing
+        }
+    }
 }
