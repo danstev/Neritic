e460486acefa61cd7881c267f8aac32de605ac30
Dan Ward <dan.steve@hotmail.co.uk>
2017-03-27 19:40:04 +0100

    ed revamp to do

--- Assets/Scripts/EnemyDrop.cs
+++ Assets/Scripts/EnemyDrop.cs
@@ -9,6 +9,8 @@ public class EnemyDrop : MonoBehaviour {
     public GameObject rareDrop;
     public GameObject veryRareDrop;
 
+    //Revamp using different objects
+
     // Use this for initialization
     void Start () {
 	
@@ -23,7 +25,6 @@ public class EnemyDrop : MonoBehaviour {
     {
         print("Item Drop");
         float rate = Random.Range(0f,1f);
-        print(rate);
         if(rate < 0.5f)
         {
             //Drop nothing
@@ -32,34 +33,28 @@ public class EnemyDrop : MonoBehaviour {
         {
             //Drop a normal drop
             int item = Random.Range(0,3);
-            print(item);
             if(item == 1)
             {
                 Instantiate(drop1, transform.position, transform.localRotation);
-                print(drop1.name);
             }
             else if(item == 2)
             {
                 Instantiate(drop2, transform);
-                print(drop2.name);
             }
             else if(item == 3)
             {
                 Instantiate(drop3, transform);
-                print(drop3.name);
             }
         }
         else if(rate > 0.95f)
         {
             //drop rare
             Instantiate(rareDrop, transform);
-            print(rareDrop.name);
         }
         else if(rate > 0.98f)
         {
             //drop very rare
             Instantiate(veryRareDrop, transform);
-            print(veryRareDrop.name);
         }
         else
         {
