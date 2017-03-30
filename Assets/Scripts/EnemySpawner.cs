8a6be1b3e8d5b0c17562ac71d27613c166b0ca3d
Dan Ward <dan.steve@hotmail.co.uk>
2017-03-30 21:39:59 +0100

    Enemy spawner done, implement multiple enemies?

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
 
-	// Use this for initialization
+    private GameObject enemy;
+    public GameObject enemyToSpawn;
+    public float timer;
+    private float timerTemp;
+
+
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if (enemy == null)
+        {
+            if (timerTemp <= 0)
+            {
+                enemy = Instantiate(enemyToSpawn, transform) as GameObject;      
+            }
+        }
+        else
+        {
+            timerTemp -= Time.deltaTime;
+        }
+    }
 }
