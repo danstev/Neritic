99623894c136b32838019321b0a12066e08a7a44
Dan Ward <dan.steve@hotmail.co.uk>
2017-04-11 08:28:34 +0100

    Dream planned

--- Assets/Scripts/Levels/Dream.cs
+++ Assets/Scripts/Levels/Dream.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Dream : MonoBehaviour {
+public class Dream {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private int[,] map;
+
+    public int[,] genMap()
+    {
+        //Gen start room
+        //Gen a bunch of rooms which lead to end room, no offshoots
+        //Gen end room (large with boss)
+        
+        return map;
+    }
+
+    public void setMap(int[,] m)
+    {
+        map = m;
+    }
 }
