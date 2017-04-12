fb5a7d71bca63f130510eb00284b8d254bf485b6
Dan Ward <dan.steve@hotmail.co.uk>
2017-04-12 03:02:47 +0100

    waterbed

--- Assets/Scripts/Levels/UnderWaterFlowerbed.cs
+++ Assets/Scripts/Levels/UnderWaterFlowerbed.cs
@@ -4,15 +4,229 @@ using System.Collections;
 public class UnderWaterFlowerbed {
 
     private int[,] map;
+    int x1, x2, y1, y2;
 
     public int[,] genMap()
     {
-        //Large map, large rooms, smoothed out a lot
+        //Build many large rooms
+        int x = map.GetLength(0);
+        int y = map.GetLength(1);
+        int rooms = Random.Range(8, 12);
+        //Make starting room near edge.
+        buildStartRoom();
+        buildEndRoom();
+
+
+        x1 = 0;
+        x2 = 0;
+
+        for (int i = 0; i < rooms; i++)
+        {
+            int height = Random.Range(4, 12);
+            int width = Random.Range(4, 12);
+            int offsetH = Random.Range(0, x - height - 1);
+            int offsetW = Random.Range(0, y - width - 1);
+
+            for (int q = offsetH; q <= height + offsetH; q++)
+            {
+                for (int w = offsetW; w <= width + offsetW; w++)
+                {
+                    map[q, w] = 1;
+
+                    if (w == width + offsetW - 1)
+                    {
+                        x1 = q;
+                        x2 = w;
+                        if (y1 == -1)
+                        {
+                            y1 = x1;
+                            y2 = x2;
+                        }
+                        else
+                        {
+                            buildCorridor(x1, x2, y1, y2, 1);
+                            y1 = x1;
+                            y2 = x2;
+                        }
+                    }
+                }
+            }
+
+        }
+        buildCorridor(x1, x2, map.GetLength(0) - 8 + 3, map.GetLength(1) - 8 + 3, 1);
+        //Go over and place a bunch of pillars
+        //Smooth
+        //Outskirts
         return map;
     }
 
     public void setMap(int[,] m)
     {
         map = m;
+    }
+
+    private void drawOutskirts(int thickness)
+    {
+        for (int g = 0; g < map.GetLength(0); g++)
+        {
+            for (int h = 0; h < thickness; h++)
+            {
+                if (Random.Range(0.0f, 1f) > 0.5f)
+                {
+                    map[g, h] = 0;
+                }
+            }
+        }
+
+        for (int g = 0; g < map.GetLength(0); g++)
+        {
+            for (int h = map.GetLength(1) - thickness; h < map.GetLength(1); h++)
+            {
+                if (Random.Range(0.0f, 1f) > 0.5f)
+                {
+                    map[g, h] = 0;
+                }
+            }
+        }
+
+        for (int g = map.GetLength(0) - thickness; g < map.GetLength(0); g++)
+        {
+            for (int h = 0; h < map.GetLength(1); h++)
+            {
+                if (Random.Range(0.0f, 1f) > 0.5f)
+                {
+                    map[g, h] = 0;
+                }
+            }
+        }
+
+        for (int g = 0; g < thickness; g++)
+        {
+            for (int h = 0; h < map.GetLength(1); h++)
+            {
+                if (Random.Range(0.0f, 1f) > 0.5f)
+                {
+                    map[g, h] = 0;
+                }
+            }
+        }
+    }
+
+    private void smoothMap(int smoothness)
+    {
+        int x = map.GetLength(0);
+        int y = map.GetLength(1);
+
+        for (int i = 0; i < smoothness; i++)
+        {
+            for (int g = 0; g < x; g++)
+            {
+                for (int h = 0; h < y; h++)
+                {
+                    int count = 0;
+
+                    if (g - 1 < 0 || g + 1 >= map.GetLength(0) || h - 1 < 0 || h + 1 >= map.GetLength(1))
+                    {
+                        map[g, h] = 1;
+                        continue;
+                    }
+
+                    if (map[g + 1, h] == 1)
+                    {
+                        count++;
+                    }
+
+                    if (map[g - 1, h] == 1)
+                    {
+                        count++;
+                    }
+
+                    if (map[g, h + 1] == 1)
+                    {
+                        count++;
+                    }
+
+                    if (map[g, h - 1] == 1)
+                    {
+                        count++;
+                    }
+
+                    if (map[g + 1, h + 1] == 1)
+                    {
+                        count++;
+                    }
+
+                    if (map[g - 1, h + 1] == 1)
+                    {
+                        count++;
+                    }
+
+                    if (map[g + 1, h - 1] == 1)
+                    {
+                        count++;
+                    }
+
+                    if (map[g - 1, h - +1] == 1)
+                    {
+                        count++;
+                    }
+
+                    if (count > 4)
+                    {
+                        map[g, h] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private void buildCorridor(int x1, int x2, int y1, int y2, int tileType)
+    {
+        //bool dir = false; //false = horizontal, true = vertical Not implemented yet
+
+        //check input
+        if (x1 > y1)
+        {
+            int t1, t2;
+            t1 = x1;
+            t2 = x2;
+
+            x1 = y1;
+            x2 = y2;
+
+            y1 = t1;
+            y2 = t2;
+        }
+
+        int middle = (x1 + y1) / 2;
+
+        for (int h = x1; h < middle; h++)
+        {
+            map[h, x2] = tileType;
+        }
+
+        for (int h = middle; h < y1; h++)
+        {
+            map[h, y2] = tileType;
+        }
+
+        if (x2 < y2)
+        {
+            for (int h = x2; h < y2; h++)
+            {
+                map[middle, h] = tileType;
+            }
+        }
+        else
+        {
+            for (int h = y2; h < x2; h++)
+            {
+                map[middle, h] = tileType;
+            }
+        }
+
+        map[middle, x2] = tileType;
+        map[middle, y2] = tileType;
+
     }
 }
