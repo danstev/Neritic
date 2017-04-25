c66910b12f9ecc9855c9c378833035f4f96084c9
Dan Ward <dan.steve@hotmail.co.uk>
2017-04-25 13:31:00 +0100

    added wall and ceiling types

--- Assets/Scripts/Levels/Tile.cs
+++ Assets/Scripts/Levels/Tile.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class Tile
 {
-
     public GameObject floorTile;
     public int xPosition;
     public int yPosition;
-    public bool ceiling;
     public GameObject ceilingTile;
+    public GameObject wallTile;
     public bool torch;
     public GameObject torchObject;
     public bool entity;
     public GameObject entityObject;
     public float spacemod = 1f;
-
-   
-
 }
