530f04e5e78c1aa2f64fe28b61c96e4f8d30fa15
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-12 06:26:36 +0100

    change scene trigger

--- Assets/Scripts/ExitLevel.cs
+++ Assets/Scripts/ExitLevel.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitLevel : MonoBehaviour {
 
+    private string level;
 
+    public void setLevel(string l)
+    {
+        level = l;
+    }
 
     void worldUse()
     {
-
-        print("hello");
+        print(level);
+        if(level == "testScene")
+        {
+            SceneManager.LoadScene("dream", LoadSceneMode.Single);
+        }
     }
 }
