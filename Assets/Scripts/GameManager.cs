bef2940dd39592b324644c638aac23c04a80a72a
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-12 03:03:35 +0100

    game manager started

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,24 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-    string level;
+    string map;
+    int level;
     float xStartPos;
     float yStartPos;
 
     void Start()
     {
+        GameObject level = GameObject.FindGameObjectWithTag("levelGen");
+        LevelGen gen = level.GetComponent<LevelGen>();
+        xStartPos = gen.getXStart();
+        yStartPos = gen.getYStart();
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         foreach(GameObject g in players)
         {
-            g.transform.position = new Vector3(xStartPos, 1, yStartPos);
+            g.transform.position = new Vector3(xStartPos, 2, yStartPos);
         }
     }
+
 
 
 	
