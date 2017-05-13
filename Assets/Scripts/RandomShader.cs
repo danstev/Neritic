f0914cb33485dde76bd2d5b17f18edd078a6cd80
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-13 11:58:36 +0100

    shader randomizer

---
+++ Assets/Scripts/RandomShader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomShader : MonoBehaviour {
+
+    public float min, max;
+	void Start () {
+        Renderer r = GetComponent<Renderer>();
+        //print(r.material.GetFloat("Magnitude"));
+        r.material.SetFloat("_Magnitude", Random.Range(min, max));
+	}
+	
+}
