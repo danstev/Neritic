7b6d7bc0e099c27309504cb49b1cbe312ec95d40
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-13 16:54:48 +0100

    unused

--- Assets/Scripts/EnemyDrop.cs
+++ Assets/Scripts/EnemyDrop.cs
@@ -23,42 +23,45 @@ public class EnemyDrop : MonoBehaviour {
 
     void OnDestroy()
     {
-        print("Item Drop");
+        //Vector3 pos = new Vector3(this.transform.position.x, this.transform.position.y + 1, this.transform.position.z);
+        Vector3 pos = transform.position;
         float rate = Random.Range(0f,1f);
-        if(rate < 0.5f)
+        if (rate < 0.95f)
         {
-            //Drop nothing
-        }
-        else if(rate < 0.95f)
-        {
+            
             //Drop a normal drop
-            int item = Random.Range(0,3);
+            int item = Random.Range(0,4);
             if(item == 1)
             {
-                Instantiate(drop1, transform.position, transform.localRotation);
+                print("should drop1");
+                GameObject d = Instantiate(drop1, transform.position, Quaternion.identity) as GameObject;
+                d.transform.position = pos;
             }
             else if(item == 2)
             {
-                Instantiate(drop2, transform);
+                print("should drop2");
+                Instantiate(drop2, transform.position, Quaternion.identity);
             }
             else if(item == 3)
             {
-                Instantiate(drop3, transform);
+                print("should drop3");
+                Instantiate(drop3, transform.position, Quaternion.identity);
             }
         }
         else if(rate > 0.95f)
         {
             //drop rare
-            Instantiate(rareDrop, transform);
+            Instantiate(rareDrop, pos, Quaternion.identity);
         }
         else if(rate > 0.98f)
         {
             //drop very rare
-            Instantiate(veryRareDrop, transform);
+            Instantiate(veryRareDrop, pos, Quaternion.identity);
         }
         else
         {
             //Drop nothing
         }
+        print("done");
     }
 }
