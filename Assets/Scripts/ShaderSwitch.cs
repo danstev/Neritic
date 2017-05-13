f31cc3a7be13c024498a1042b16229c975d77a82
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-13 11:58:48 +0100

    not used

---
+++ Assets/Scripts/ShaderSwitch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShaderSwitch : MonoBehaviour {
+
+    public Shader s;
+	// Use this for initialization
+	void Start () {
+        Camera.main.SetReplacementShader(s, "RenderType");
+    }
+	
+
+}
