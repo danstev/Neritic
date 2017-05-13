3193b8a2d233a60dd5b84352fc052f21e430220d
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-13 11:57:29 +0100

    exit level script added

--- Assets/Scripts/ExitLevel.cs
+++ Assets/Scripts/ExitLevel.cs
@@ -18,5 +18,21 @@ public class ExitLevel : MonoBehaviour {
         {
             SceneManager.LoadScene("dream", LoadSceneMode.Single);
         }
+        else if(level == "dream")
+        {
+            SceneManager.LoadScene("forest", LoadSceneMode.Single);
+        }
+        else if (level == "forest")
+        {
+            SceneManager.LoadScene("dungeon", LoadSceneMode.Single);
+        }
+        else if (level == "dungeon")
+        {
+            SceneManager.LoadScene("endLevel", LoadSceneMode.Single);
+        }
+        else if (level == "endLevel")
+        {
+            SceneManager.LoadScene("endScreen", LoadSceneMode.Single);
+        }
     }
 }
