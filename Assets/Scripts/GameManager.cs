633ce09c657b58774147bebcc86e35e893fb7752
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-16 11:39:32 +0100

    game manager changes on level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,32 +25,81 @@ public class GameManager : MonoBehaviour {
         print(xStartPos);
         print(yStartPos);
 
+        GameObject player;
+        Statistics stats;
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         foreach(GameObject g in players)
         {
             g.transform.position = new Vector3(xStartPos, 2, yStartPos);
+            player = g;
+            stats = g.GetComponent<Statistics>();
+            GameObject rain = player.transform.Find("Rain").gameObject;
+            Inventory i = player.GetComponent<Inventory>();
+            if (map == "dream")
+            {
+                GameObject sword = Instantiate(Resources.Load("Prefabs/Weapons/SmallSword")) as GameObject;
+                i.AddItem(sword);
+                //god stats etc
+                stats.maxHealth = 500;
+                stats.curHealth = 500;
+                stats.maxMana = 250;
+                stats.curMana = 250;
+                stats.attack = 100;
+                stats.magicAttack = 100;
+                stats.strength = 20;
+                stats.intellect = 20;
+                stats.agility = 20;
+                PlayerControl cont = player.GetComponent<PlayerControl>();
+                cont.refreshStats();
+                stats.level = 10;
+                i.updateAllStatisitics();
+                rain.SetActive(false);
+
+            }
+            else if (map == "forest")
+            {
+                //check if sword is equipped
+                if(i.equipped[0] == null)
+                {
+                    GameObject sword = Instantiate(Resources.Load("Prefabs/Weapons/SmallSword")) as GameObject;
+                    i.AddItem(sword);
+                }
+
+                
+                //rain on, drop all items
+                stats.maxHealth = 100;
+                stats.curHealth = 100;
+                stats.maxMana = 50;
+                stats.curMana = 50;
+                stats.attack = 10;
+                stats.magicAttack = 25;
+                stats.strength = 5;
+                stats.intellect = 5;
+                stats.agility = 10;
+                PlayerControl cont = player.GetComponent<PlayerControl>();
+                cont.refreshStats();
+                i.updateAllStatisitics();
+                stats.level = 1;
+                rain.SetActive(true);
+
+            }
+            else if (map == "dungeon")
+            {
+                //rain off
+                rain.SetActive(false);
+            }
+            else if (map == "endLevel")
+            {
+                //rain on
+                rain.SetActive(true);
+            }
         }
 
         GameObject e = GameObject.FindGameObjectWithTag("LevelSwitch");
         ExitLevel exit = e.GetComponent<ExitLevel>();
         exit.setLevel(map);
 
-        if(map == "dream")
-        {
-            //god stats etc
-        }
-        else if(map == "forest")
-        {
-            //basic stats no inv
-        }
-        else if(map == "dungeon")
-        {
-            //nothin?
-        }
-        else if(map == "endLevel")
-        {
-            //nothin?
-        }
+        
     }
 
 
