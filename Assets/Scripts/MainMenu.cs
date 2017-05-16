9b49457156b685c4a8e05d9ce8108d491b25770b
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-16 11:40:07 +0100

    main menu works

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,17 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+    bool menu = true;
+    bool help = false;
+    GUIStyle style;
+
 	// Use this for initialization
 	void Start () {
         Cursor.lockState = CursorLockMode.None;
+        style = new GUIStyle();
+        style.wordWrap = true;
+        
+
     }
 	
 	// Update is called once per frame
@@ -16,24 +24,51 @@ public class MainMenu : MonoBehaviour {
 
     void OnGUI ()
     {
-        GUI.Box(new Rect(70 + 30, 30, 50, 50), "hello"); //draw buttons here
-
-        //Start game
-        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "Start Game"))
+        int w = Screen.width / 12;
+        int h = Screen.height / 12;
+        if (menu)
         {
-            GameObject player = Resources.Load("Prefabs/Player/Player") as GameObject;
-            Instantiate(player);
-            SceneManager.LoadScene("dream");
+            
+            //Start game
+            if (GUI.Button(new Rect(w * 6 - (w / 2), h * 6 - (h * 4), 200, 50), "Start Game"))
+            {
+                GameObject player = Resources.Load("Prefabs/Player/Player") as GameObject;
+                Instantiate(player);
+                SceneManager.LoadScene("dream");
+            }
+            //skip tut
+            if (GUI.Button(new Rect(w * 6 - (w / 2), h * 6 - (h * 3), 200, 50), "Start Game but skip\n tutorial level"))
+            {
+                GameObject player = Resources.Load("Prefabs/Player/Player") as GameObject;
+                Instantiate(player);
+                SceneManager.LoadScene("forest");
+            }
+            //Help
+            if (GUI.Button(new Rect(w * 6 - (w / 2), h * 6 - (h * 2), 200, 50), "Controls"))
+            {
+                menu = false;
+                help = true;
+                
+            }
+            //exit
+            if (GUI.Button(new Rect(w * 6 - (w / 2), h * 6 - (h * 1), 200, 50), "Exit Game"))
+            {
+                Application.Quit();
+            }
         }
-        //Help
-        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 25, 100, 50), "Tutorial"))
+        else if(help)
         {
-            //nothing atm lol
-        }
-        //exit
-        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 75, 100, 50), "Exit Game"))
-        {
-            Application.Quit();
+            
+            GUI.Box(new Rect(w * 6 - (w /2), h * 6 - (h * 4), w, h), "Controls");
+            GUI.backgroundColor = Color.blue;
+            GUI.Box(new Rect(w * 6 - (w ), h * 6 - (h * 3), w * 2, h), "Use WASD or the arrow \nkeys to move around and the mouse to aim.");
+            GUI.Box(new Rect(w * 6 - (w ), h * 6 - (h * 2), w * 2, h), "Clicking swings your \nsword, \"F\" fires a fire spell.");
+            GUI.Box(new Rect(w * 6 - (w ), h * 6 - (h * 1), w * 2, h), "Use tab to open inventory, \n\"U\" to open equipment and \n\"Escape\" to open the \nmain menu.");
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                menu = true;
+                help = false;
+            }
         }
         
     }
