9c213d629ad9256727ed3790ddbba3f0618b42bb
Dan Ward <dan.steve@hotmail.co.uk>
2017-05-30 15:25:15 +0100

    defucnt fuck me

---
+++ Assets/Scripts/Disable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Disable : NetworkBehaviour{
+
+	// Use this for initialization
+	void Start () {
+
+        if( !isLocalPlayer )
+        {
+            gameObject.SetActive(false);
+        }
+	
+	}
+	
+
+}
