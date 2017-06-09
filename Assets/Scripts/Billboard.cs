c2da94d86dc66deae9edc5a1f46b4b3d13f89954
Dan Ward <dan.steve@hotmail.co.uk>
2017-06-09 18:24:17 +0100

    looks more at player now

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -6,6 +6,6 @@ public class Billboard : MonoBehaviour
 
     void Update() 
 	{
-        transform.LookAt( new Vector3 (Camera.main.transform.position.x, 0.5f, Camera.main.transform.position.z), Vector3.up);
+        transform.LookAt( new Vector3 (Camera.main.transform.position.x, 0.75f, Camera.main.transform.position.z), Vector3.up);
     }
 }
