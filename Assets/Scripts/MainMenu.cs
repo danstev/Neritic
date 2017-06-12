7775931602def9ad150f660924eba6b8e246ff90
Dan Ward <dan.steve@hotmail.co.uk>
2017-06-12 20:24:21 +0100

    test level for testing

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -51,9 +51,16 @@ public class MainMenu : MonoBehaviour {
                 
             }
             //exit
-            if (GUI.Button(new Rect(w * 6 - (w / 2), h * 6 - (h * 1), 200, 50), "Exit Game"))
+            if (GUI.Button(new Rect(w * 6 - (w / 2), h * 6 - (h), 200, 50), "Exit Game"))
             {
                 Application.Quit();
+            }
+
+            if (GUI.Button(new Rect(w * 6 - (w / 2), h * 6, 200, 50), "Test Level"))
+            {
+                GameObject player = Resources.Load("Prefabs/Player/Player") as GameObject;
+                Instantiate(player);
+                SceneManager.LoadScene("testScene");
             }
         }
         else if(help)
@@ -64,6 +71,7 @@ public class MainMenu : MonoBehaviour {
             GUI.Box(new Rect(w * 6 - (w ), h * 6 - (h * 3), w * 2, h), "Use WASD or the arrow \nkeys to move around and the mouse to aim.");
             GUI.Box(new Rect(w * 6 - (w ), h * 6 - (h * 2), w * 2, h), "Clicking swings your \nsword, \"F\" fires a fire spell.");
             GUI.Box(new Rect(w * 6 - (w ), h * 6 - (h * 1), w * 2, h), "Use tab to open inventory, \n\"U\" to open equipment and \n\"Escape\" to open the \nmain menu.");
+
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 menu = true;
