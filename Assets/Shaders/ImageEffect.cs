7db078e4a5449f61c614959c9c197d673a4f90c6
Dan Ward <dan.steve@hotmail.co.uk>
2017-07-12 11:16:20 +0100

    Image effect script, applies a material to camera

---
+++ Assets/Shaders/ImageEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ImageEffect : MonoBehaviour {
+
+    public Material EffectMaterial;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, EffectMaterial);
+    }
+
+
+}
