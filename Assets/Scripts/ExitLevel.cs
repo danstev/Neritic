d07a57f284dc1c98c51194c114ca7381ed25cc38
Dan Ward <dan.steve@hotmail.co.uk>
2017-07-17 20:09:14 +0100

    exitlevel added custom out

--- Assets/Scripts/ExitLevel.cs
+++ Assets/Scripts/ExitLevel.cs
@@ -4,35 +4,43 @@ using UnityEngine.SceneManagement;
 
 public class ExitLevel : MonoBehaviour {
 
-    private string level;
+    public string level;
+    public bool manual = true;
 
     public void setLevel(string l)
     {
         level = l;
+        manual = false;
     }
 
     void worldUse()
     {
-        print(level);
-        if(level == "testScene")
+        if (manual)
         {
-            SceneManager.LoadScene("dream", LoadSceneMode.Single);
+            SceneManager.LoadScene(level, LoadSceneMode.Single);
         }
-        else if(level == "dream")
+        else
         {
-            SceneManager.LoadScene("forest", LoadSceneMode.Single);
-        }
-        else if (level == "forest")
-        {
-            SceneManager.LoadScene("dungeon", LoadSceneMode.Single);
-        }
-        else if (level == "dungeon")
-        {
-            SceneManager.LoadScene("endLevel", LoadSceneMode.Single);
-        }
-        else if (level == "endLevel")
-        {
-            SceneManager.LoadScene("endScreen", LoadSceneMode.Single);
+            if (level == "testScene")
+            {
+                SceneManager.LoadScene("dream", LoadSceneMode.Single);
+            }
+            else if (level == "dream")
+            {
+                SceneManager.LoadScene("forest", LoadSceneMode.Single);
+            }
+            else if (level == "forest")
+            {
+                SceneManager.LoadScene("dungeon", LoadSceneMode.Single);
+            }
+            else if (level == "dungeon")
+            {
+                SceneManager.LoadScene("endLevel", LoadSceneMode.Single);
+            }
+            else if (level == "endLevel")
+            {
+                SceneManager.LoadScene("endScreen", LoadSceneMode.Single);
+            }
         }
     }
 }
