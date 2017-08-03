01cfd7c36d95a11592aaeb1d2e8a13c82e754f6f
Dan Ward <dan.steve@hotmail.co.uk>
2017-08-03 21:20:32 +0100

    neat

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -12,19 +12,14 @@ public class Box : MonoBehaviour {
 
     void worldUse()
     {
-        //For item in inventory, detatch from parent, make real
         float yPos = 0;
         foreach (GameObject g in i.slots)
         {
-            //Detatch from parent
             g.transform.parent = null;
-            //activate the object
             g.SetActive(true);
             g.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + yPos,this.transform.position.z);
             yPos += 0.25f;
         }
-
-        //Destroy chest
         Destroy(this);
     }
 }
