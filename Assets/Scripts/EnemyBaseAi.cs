61ca72ca1c8a761c78c5f13c0d5fcbc741ba6f41
Dan Ward <dan.steve@hotmail.co.uk>
2017-08-08 08:26:35 +0100

    Magic now spawns closer to enemies

--- Assets/Scripts/EnemyBaseAi.cs
+++ Assets/Scripts/EnemyBaseAi.cs
@@ -232,7 +232,7 @@ public class EnemyBaseAi : MonoBehaviour {
             {
                 attackTimer = stats.magicTime;
                 GameObject spell;
-                spell = Instantiate(stats.magicSpell, transform.position + transform.forward * 1, transform.rotation) as GameObject;
+                spell = Instantiate(stats.magicSpell, transform.position + transform.forward * 0.1f, transform.rotation) as GameObject;
                 Rigidbody spellR = spell.GetComponent<Rigidbody>();
                 spellR.AddForce(spell.transform.forward * stats.magicSpeed);
                 Spell spellA = spell.GetComponent<Spell>();
