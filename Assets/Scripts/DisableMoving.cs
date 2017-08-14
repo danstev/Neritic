a6fde58378d0a47679594e19909e0bb706ccbc85
Dan Ward <dan.steve@hotmail.co.uk>
2017-08-14 09:19:37 +0100

    testing efficient disab9ing

---
+++ Assets/Scripts/DisableMoving.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableMoving : MonoBehaviour {
+
+
+    void OnColliderEnter(Collider col)
+    {
+        col.gameObject.SetActive(true);
+    }
+
+    void OnColliderLeave(Collider col)
+    {
+        col.gameObject.SetActive(false);
+    }
+}
