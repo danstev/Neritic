549b2b130972130d33bc4de605d3ee37cf6437f0
Dan Ward <dan.steve@hotmail.co.uk>
2017-08-24 21:19:29 +0100

    clean

--- Assets/Scripts/Levels/Dungeon2.cs
+++ Assets/Scripts/Levels/Dungeon2.cs
@@ -93,7 +93,6 @@ public class Dungeon2 {
                 //up, down, left right
                 buildCorridor(xMid[u], yMid[u], xMid[u - 1], yMid[u - 1], 1);
                 buildCorridor(xMid[u], yMid[u], xMid[u + 1], yMid[u + 1], 1);
-                //buildCorridor(xMid[u], yMid[u], xMid[u - rooms], yMid[u - rooms], 1);
                 buildCorridor(xMid[u], yMid[u], xMid[u + rooms], yMid[u + rooms], 1);
             }
 
